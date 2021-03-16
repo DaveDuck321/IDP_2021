a9c6fd18ccb6399992fb3ef23f054e88305b445e
DaveDuck321 <tg443@cam.ac.uk>
2021-03-16 17:00:15 -0500

    Fix Felix code #4

--- Optimal_solution/controllers/robot_controller/block_deposit.py
+++ Optimal_solution/controllers/robot_controller/block_deposit.py
@@ -1,6 +1,6 @@
 from common import util
 
-
+DROPOFF_ANGLE_TOLERANCE = 5 / 360 * 3  # Tolerance is 5 degrees
 DROPOFF_RADIUS = 0.17
 DROPOFF_LOCATIONS = {"Small": (0.0, 0.4), "Fluffy": (0.0, -0.4)}
 
@@ -13,14 +13,25 @@ class BlockDeposit:
         self.pincer_controller = pincer_controller
 
         self.dropoff_target = DROPOFF_LOCATIONS[robot_name]
+        self.opening_pincer = False
 
     def __call__(self):
+        if self.opening_pincer:
+            return self.pincer_controller.open_pincer()
+
         current_position = self.positioning_system.get_2D_position()
-        self.drive_controller.navigate_toward_point(self.positioning_system, self.dropoff_target)
 
         if util.get_distance(current_position, self.dropoff_target) < DROPOFF_RADIUS:
-            self.drive_controller.halt()
-            if self.pincer_controller.open_pincer():
-                return True
+            is_aligned = self.drive_controller.turn_toward_point(
+                self.positioning_system,
+                self.dropoff_target,
+                DROPOFF_ANGLE_TOLERANCE
+            )
+            if is_aligned:
+                self.drive_controller.halt()
+                self.opening_pincer = True
+
+        else:
+            self.drive_controller.navigate_toward_point(self.positioning_system, self.dropoff_target)
 
         return False
